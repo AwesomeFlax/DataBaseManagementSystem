77080e17e6bbea56e89f89c7f75564bf9b56ccd3
krutk <artstyleflax@gmail.com>
2017-03-14 14:50:58 +0100

    init commit

---
+++ DataBaseManagementSystem/Connection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+
+namespace DataBaseManagementSystem
+{
+    class Connection
+    {
+        OleDbConnection conn;
+
+        public OleDbDataAdapter fillDataWeGetBefore(String table_name)
+        {
+            OleDbCommand com = new OleDbCommand("SELECT * FROM " + table_name, conn);
+            OleDbDataAdapter dataAdapter = new OleDbDataAdapter(com);
+
+            return dataAdapter;
+        }
+
+        public DataSet fillDataSet(String dataBasePath)
+        {
+            conn = new OleDbConnection(dataBasePath);
+            conn.Close();
+            conn.Open();
+
+            DataSet ds = new DataSet();
+            ds.Tables.Clear();
+            ds.Tables.Add(conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" }));
+
+            return ds;
+        }
+
+        public System.Data.DataTable loadDataBase(String dataBasePath)
+        {
+            System.Data.DataTable t1 = null;
+
+            try
+            {
+                DataSet ds = fillDataSet(dataBasePath);
+
+                foreach (DataRow r in ds.Tables[0].Rows)
+                {
+                    t1 = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Columns,
+                    new object[] { null, null, r["TABLE_NAME"], null });
+                }
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open) conn.Close();
+            }
+
+            return t1;
+        }
+    }
+}
--- DataBaseManagementSystem/MainWindow.xaml.cs
+++ DataBaseManagementSystem/MainWindow.xaml.cs
@@ -1,28 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.IO;
+using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DataBaseManagementSystem
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        Connection con = new Connection();
+        static String connectionPath = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\test.mdb";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataSet ds = con.fillDataSet(connectionPath);
+            System.Data.DataTable t;
+
+            foreach (DataRow r in ds.Tables[0].Rows)
+            {
+                t = con.loadDataBase(connectionPath);
+                t.TableName = r["TABLE_NAME"].ToString();
+                tablesList.Items.Add(t.TableName);
+            }
+        }
+
+        // при выборе пункта combox грузит соответствующую таблицу
+        private void tablesList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            OleDbDataAdapter dataAdapter = con.fillDataWeGetBefore(tablesList.SelectedValue.ToString());
+            DataTable dataTable = new DataTable();
+
+            dataAdapter.Fill(dataTable);
+            mainDataGrid.ItemsSource = dataTable.DefaultView;
         }
     }
 }
